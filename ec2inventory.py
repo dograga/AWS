b9a1d275bdf5261c4187881b3b8fe177bc013b15
root <root@ip-10-40-157-7.ec2.internal>
2018-01-30 02:59:24 +0000

    EC2 & RDS inventory

---
+++ ec2inventory.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python
+import boto3
+from datetime import datetime
+from datetime import timedelta
+from time import gmtime, strftime
+import os
+import pprint
+from bson import json_util
+import json
+
+
+class awsec2inventory():
+     def __init__(self):
+         self.regions=["us-east-1"]
+         self.inventory=[]
+         self.acctid=boto3.client('sts').get_caller_identity().get('Account')
+
+     def instancekey(self,inst,key):
+         ## Module to handle potential unassigned value
+         try:
+             val=inst[key]
+         except:
+             val=""
+         return val
+
+     def updateec2json(self,inst,resid):
+         output={}
+         output['ReservationId']=resid
+         #print inst
+         output["AccountId"]=self.acctid
+         output['Platform']=self.instancekey(inst,'Platform')
+         output['PublicIpAddress']=self.instancekey(inst,'PublicIpAddress')
+         output['InstanceType']=self.instancekey(inst,'InstanceType')
+         output['AvailabilityZone']=self.instancekey(inst,'AvailabilityZone')
+         output['ImageId']=self.instancekey(inst,'ImageId')
+         output['Monitoring']=inst['Monitoring']['State']
+         output['PrivateIpAddress']=inst['PrivateIpAddress']
+         output['State']=inst['State']['Name']
+         output['LaunchTime']=inst['LaunchTime']
+         output['VpcId']=inst['VpcId']
+         output['InstanceId']=inst['InstanceId']
+         try:
+             output['tag']=[tag['Value'] for tag in inst['Tags']]
+         except:
+             output['tag']=""
+         secgroups=""
+         if output['State'] =='running':
+             securityGroups = inst['SecurityGroups']
+             secgroups = [secgroups['GroupName'] for secgroups in securityGroups]
+             output['SecurityGroups']=secgroups
+         self.inventory.append(output)
+
+
+     def getinstanceinfo(self):
+         for region in self.regions:
+             ec = boto3.client('ec2',region)
+             ecinventory    = ec.describe_instances()
+             reservations = ecinventory.get('Reservations',[])
+             for reservation in reservations:
+                 resid = reservation['ReservationId']
+                 instances=[i for i in reservation['Instances']]
+                 for inst in instances:
+                    try:
+                        self.updateec2json(inst,resid)
+                    except Exception as e:
+                        print "Failed to update info "+str(e.args)+e.message
+
+         inventory=json.loads(json.dumps(self.inventory,default=json_util.default))
+         for i in inventory:
+              print "============================"
+              pprint.pprint(i)
+
+a=awsec2inventory()
+a.getinstanceinfo()
---
+++ rdsinventory.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+import boto3
+from datetime import datetime
+from datetime import timedelta
+from time import gmtime, strftime
+import os
+import pprint
+from bson import json_util
+import json
+
+
+class awsrdsinventory():
+     def __init__(self):
+         self.regions=["us-east-1"]
+         self.inventory=[]
+         self.acctid=boto3.client('sts').get_caller_identity().get('Account')
+
+     def instancekey(self,inst,key):
+         ## Module to handle potential unassigned value
+         try:
+             val=inst[key]
+         except:
+             val=""
+         return val
+
+
+     def updatedbjson(self,dbinstance):
+           #print dbinstance
+           output={}
+           output["AccountId"]=self.acctid
+           output["PubliclyAccessible"]=dbinstance["PubliclyAccessible"]
+           output["MasterUsername"]=dbinstance["MasterUsername"]
+           output["MonitoringInterval"]=dbinstance["MonitoringInterval"]
+           output["LicenseModel"]=dbinstance["LicenseModel"]
+           output["InstanceCreateTime"]=dbinstance["InstanceCreateTime"]
+           output["AutoMinorVersionUpgrade"] =dbinstance["AutoMinorVersionUpgrade"]
+           output["PreferredBackupWindow"] =dbinstance["PreferredBackupWindow"]
+           output["AllocatedStorage"]=dbinstance["AllocatedStorage"]
+           output["DBInstanceArn"]=dbinstance["DBInstanceArn"]
+           output["BackupRetentionPeriod"] =dbinstance["BackupRetentionPeriod"]
+           output['DBName']=self.instancekey(dbinstance,'DBName')
+           output["PreferredMaintenanceWindow"]=dbinstance["PreferredMaintenanceWindow"]
+           output['Port']=dbinstance['Endpoint']['Port']
+           output['Address']=dbinstance['Endpoint']['Address']
+           output["DBInstanceStatus"]=dbinstance["DBInstanceStatus"]
+           output["IAMDatabaseAuthenticationEnabled"] =dbinstance["IAMDatabaseAuthenticationEnabled"]
+           output["AvailabilityZone"]=dbinstance["AvailabilityZone"]
+           output["StorageEncrypted"]=dbinstance["StorageEncrypted"]
+           output["DBInstanceClass"]=dbinstance["DBInstanceClass"]
+           output["DbInstancePort"]=dbinstance["DbInstancePort"]
+           output["DBInstanceIdentifier"]=dbinstance["DBInstanceIdentifier"]
+           self.inventory.append(output)
+
+
+     def getinstanceinfo(self):
+         for region in self.regions:
+             rds = boto3.client('rds',region)
+             rds    = rds.describe_db_instances()
+             dbinstances=rds['DBInstances']
+             for dbinstance in dbinstances:
+                  #print dbinstance
+                try:
+                    self.updatedbjson(dbinstance)
+                except Exception as e:
+                    print "Failed to update info "+str(e.args)+e.message
+         inventory=json.loads(json.dumps(self.inventory,default=json_util.default))
+         for i in inventory:
+              print "============================"
+              pprint.pprint(i)
+
+
+r=awsrdsinventory()
+r.getinstanceinfo()
